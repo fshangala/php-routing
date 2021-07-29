cdf238fe6ad71ac238c5c08ba8843521442a8c00
fshangala <fshangala@gmail.com>
2021-07-29 12:49:29 +0200

    am stack

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ include_once 'Router.php';
 $router = new Router(new Request);
 
 $router->get('/', function(){
-    return <<<HTML
-    <h1>Hello world</h1>
-    HTML;
+    return '';
 });
 
 $router->get('/home', function(){
---
+++ templates/home.php
@@ -0,0 +1,11 @@
+<?php
+<<<HTML
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Hellow me</body>
+
+</body>
+</html>
+HTML;
